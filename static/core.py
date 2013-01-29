f150b9a0c6224fd22a41676a3d64dce07cedc454
MOON_CLJ <lijunli2598@gmail.com>
2013-01-29 10:39:36 +0800

    tmp 仓库更新完成

--- static/core.py
+++ static/core.py
@@ -4,8 +4,12 @@
 
 from clint import args
 from clint.textui import puts, indent, colored
-from .utils import get_app_root, get_static_conf, gen_repo_conf, NotFoundAppRoot, defaultHostDict
+from .utils import get_app_root, get_static_conf, gen_repo_conf, \
+    dir_dir_modified, file_dir_modified, file_file_modified, \
+    get_remote_url, clone, fetch, add_tmpdir2gitignore, \
+    NotFoundAppRoot, defaultHostDict
 import simplejson as json
+import shutil
 import sys
 import os
 
@@ -37,12 +41,6 @@ def pull():
             puts(colored.red('"staticpy" key not in static.json'))
         sys.exit(1)
 
-    # 获取缺省值
-    for k in ('js_dir', 'css_dir', 'pic_dir'):
-        if static_conf.get(k):
-            global_conf[k] = static_conf.get(k)
-            del static_conf[k]
-
     global_conf['host_dict'] = defaultHostDict
     if 'hostDict' in static_conf:
         for k, v in static_conf['hostDict'].iteritems():
@@ -56,7 +54,31 @@ def pull():
 
         # 继承缺省静态文件路径
         repo_conf = gen_repo_conf(global_conf, conf_json, repo_name)
-        print repo_conf
+
+        # 记录tmp仓库更新前,本地相对于仓库有修改的文件
+        local_mdfied = set()
+        if os.path.exists(repo_conf['tmpdir']) and 'file' in repo_conf:
+            for source, target in repo_conf['file'].iteritems():
+                if os.path.isdir(source):
+                    if os.path.isdir(target):
+                        local_mdfied |= dir_dir_modified(source, target)
+                elif os.path.isfile(source):
+                    if os.path.isdir(target):
+                        local_mdfied |= file_dir_modified(source, target)
+                    elif os.path.isfile(target):
+                        local_mdfied |= file_file_modified(source, target)
+
+        # 更新tmp仓库，注意如果某个repo的host变了而repo名没变，检查remote url
+        # 首先将.staticpytmp 添加到.gitignore
+        add_tmpdir2gitignore(global_conf['app_root'])
+        if os.path.exists(repo_conf['tmpdir']):
+            if repo_conf['host'] == get_remote_url(repo_conf['tmpdir']):
+                fetch(repo_conf['tmpdir'])
+            else:
+                shutil.rmtree(repo_conf['tmpdir'])
+                clone(repo_conf['host'], repo_conf['tmpdir'])
+        else:
+            clone(repo_conf['host'], repo_conf['tmpdir'])
 
 
 def main():
--- static/utils.py
+++ static/utils.py
@@ -1,5 +1,11 @@
+from subprocess import Popen, PIPE
+from clint.utils import tsplit
+import subprocess
 import os
+import errno
 import simplejson as json
+import time
+import difflib
 
 
 defaultHostDict = {
@@ -27,18 +33,82 @@ def get_static_conf(app_root):
 
 def gen_repo_conf(global_conf, conf_json, repo_name):
     repo_conf = {}
-    # 继承缺省静态文件路径
-    for k in ('js_dir', 'css_dir', 'pic_dir'):
-        repo_conf[k] = conf_json.get(k, global_conf.get(k))
-        if repo_conf[k]:
-            repo_conf[k] = os.path.join(global_conf['app_root'], repo_conf[k].lstrip('/'))
-
-    # 读取特有配置
     repo_conf['host'] = global_conf['host_dict'][conf_json['host']] if 'host' in conf_json else global_conf['host_dict']['github']
     repo_conf['host'] = repo_conf['host'] % repo_name
     repo_conf['commit'] = conf_json.get('commit') or conf_json.get('tag')
     repo_conf['build'] = conf_json.get('build')
+    repo_conf['file'] = conf_json.get('file')
+    repo_conf['tmpdir'] = os.path.join(global_conf['app_root'], '.staticpytmp/%s' % repo_name)
+
     return repo_conf
+
+
+def get_remote_url(repo_dir):
+    """假定只有一个remote url,返回它"""
+    cwd = os.getcwd()
+    os.chdir(repo_dir)
+    r = Popen(['git', 'remote', '-v'], stdout=PIPE).communicate()[0]
+    os.chdir(cwd)
+    remote_url = tsplit(r, ['\n', '\t', ' '])[1]
+
+    return remote_url
+
+
+def is_file_modified(source_file, target_file):
+    """返回两个文件是否有diff, 若target_file不存在,返回None, 没有diff返回[]"""
+    if not os.path.exists(target_file):
+        return None
+
+    fromlines = open(source_file, 'U').readlines()
+    tolines = open(target_file, 'U').readlines()
+    fromdate = time.ctime(os.stat(source_file).st_mtime)
+    todate = time.ctime(os.stat(target_file).st_mtime)
+
+    return list(difflib.context_diff(fromlines, tolines, source_file, target_file, fromdate, todate))
+
+
+def dir_dir_modified(source_dir, target_dir):
+    """返回source_dir里对应target_dir有修改的文件路径集合"""
+    modified_files = set()
+    for root, dirs, files in os.walk(source_dir):
+        for f in files:
+            source_file = os.path.join(root, f)
+            modified_files |= file_dir_modified(source_file, target_dir)
+
+    return modified_files
+
+
+def file_dir_modified(source_file, target_dir):
+    """返回source_file在target_dir对应文件有修改的文件路径集合"""
+    target_file = os.path.join(target_dir, os.path.split(source_file)[-1])
+    return file_file_modified(source_file, target_file)
+
+
+def file_file_modified(source_file, target_file):
+    """返回source_file与target_file对应文件有修改的文件路径集合"""
+    return set([source_file]) if is_file_modified(source_file, target_file) else set()
+
+
+def clone(remote_url, tmp_repo_dir):
+    args = ['git', 'clone', remote_url, tmp_repo_dir]
+    print subprocess.check_output(args)
+
+
+def fetch(tmp_repo_dir):
+    args = ['git', 'fetch', tmp_repo_dir]
+    print subprocess.check_output(args)
+
+
+def add_tmpdir2gitignore(app_root):
+    gitignore_path = os.path.join(app_root, '.gitignore')
+    try:
+        ignores = open(gitignore_path, 'U').readlines()
+        if not '.staticpytmp\n' in ignores:
+            ignores.append('.staticpytmp\n')
+            open(gitignore_path, 'w').write(''.join(ignores))
+    except IOError, e:
+        if e.errno == errno.ENOENT:
+            pass
 
 
 class NotFoundAppRoot(IOError):
